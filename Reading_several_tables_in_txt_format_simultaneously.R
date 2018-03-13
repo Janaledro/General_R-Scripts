a4268e8fb070443f18a7a2713fa99004387018f8
Janaledro <alejandro.garfide@ciencias.unam.mx>
2018-03-12 19:46:22 -0600

    Script para montar varias tablas a la vez en R

---
+++ Reading_several_tables_in_txt_format_simultaneously.R
@@ -0,0 +1,9 @@
+##Cargando todas las tablas para generar una sola
+#POR MEDIO DE ESTE SCRIPT, SE ES POSIBLE LEER VARIAS TABLAS (EN ESTE CASO, 10)
+#SIEMPRE Y CUANDO TODAS TENGAN EL MISMO NÚMERO DE COLUMNAS Y EL MISMO TÍTULO EN 
+#CADA UNA DE ÉSTAS.
+library(plyr)
+setwd("Lista de SNPS GWASdb para TCS/")
+file_list<-list.files(pattern = ".*.txt")
+SNPS_FOR_ADDICTION.DF = ldply(file_list, read.table, sep = "\t", header= T)
+write.csv(SNPS_FOR_ADDICTION.DF, file = "SNPS_FOR_ADDICTIONS.csv")
